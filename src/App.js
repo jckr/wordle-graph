879cce531b51812f6298d18cd6d75d37e0dbe85d
Jerome Cukier <jeromecukier+github@gmail.com>
2022-09-09 17:42:25 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './style.css';
 import data from './data';
 import Tree from './tree.js';
 
-import { solutions, validWords } from './data/words';
-import Lexicon from './lib/lexicon';
+// import { solutions, validWords } from './data/words';
+// import Lexicon from './lib/lexicon';
 
-const lexicon = new Lexicon(solutions, validWords);
-lexicon.init();
+// const lexicon = new Lexicon(solutions, validWords);
+// lexicon.init();
 
 export default function App() {
   return <Tree data={data} />;
